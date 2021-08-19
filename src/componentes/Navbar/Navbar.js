7d4f8958b9490c4ad7aed972b5152cf8394a50a9
Emiliano Rodriguez <emilianor81@gmail.com>
2021-08-19 17:13:10 -0300

    .

---
+++ src/componentes/Navbar/Navbar.js
@@ -0,0 +1,64 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import style from "./NavBar.module.css";
+import { searchRecipes, getRecipes } from '../../redux/Actions';
+import { connect } from 'react-redux';
+// import Recipe from '../../components/recipe/Recipe'
+
+
+const NavBar = (props) => {
+  const [recipe, setRecipes] = useState('');
+  // useEffect(() => {
+  //   getRecipes()
+  //   getDiets();
+  // }, [getRecipes, getDiets])
+
+  function handleChange(event) {
+    setRecipes(event.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    props.searchRecipes(recipe)
+    setRecipes('')
+  }
+
+  return (
+     <nav className={style.NavBar}>
+        <div className={style.Links}>
+            <Link to="/home" className={style.link}>Home</Link>
+            <Link to="/about" className={style.link}>About</Link>            
+            <Link to="/create" className={style.link}>Create Recipe</Link>         
+        </div>
+      <div  className={style.form}> 
+      <form onSubmit={(e) => { handleSubmit(e) }}>
+          <input className={style.input} id='SearchInput' type='text' placeholder='Ingrese su busqueda' value={recipe}
+            onChange={(e) => { handleChange(e) }}/>
+          <input className={style.boton} id='SearchSubmitButton' type='submit' value='Search' />
+        </form>
+      </div> 
+       
+      </nav> 
+  
+  )
+}
+
+
+function mapStateToProps(state) {
+  return {
+    recipes: state.searchedRecipes,
+    allrecipes: state.allRecipes,
+    alldiets: state.allDiets
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    searchRecipes: (recipe) => dispatch(searchRecipes(recipe)),
+    getRecipes: (recipe) => dispatch(getRecipes(recipe)),
+
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
---
+++ src/redux/Actions.js
@@ -0,0 +1,104 @@
+import axios from 'axios';
+
+export function getRestaurants() {
+  return function (dispatch) {
+    axios.get('http://localhost:3001/restaurants')
+      .then(res => dispatch({
+        type: 'ALL_RESTAURANTS',
+        payload: res.data
+      })
+      ).catch(err => {
+        console.error(err)
+      });
+  };
+};
+
+
+export function searchRestaurant(restaurant) {
+  if (restaurant !== '') {
+    return function (dispatch) {
+      axios.get(`http://localhost:3001/restaurant?name=${restaurant}`)
+       .then(res => dispatch({
+          type: 'SEARCH_RESTAURANT',
+          payload: res.data
+        })
+        ).catch(err => {
+          console.error(err)
+        });
+    };
+  } else {
+    return {
+      type: 'SEARCH_RESTAURANT',
+      payload: []
+    }
+  };
+};
+
+
+export function addRestaurant({ nombre, descripcion, direccion, ciudad, foto }) {
+  return function (dispatch) {
+    const Restaurant = { nombre, descripcion, direccion, ciudad, foto };
+    axios.post(`http://localhost:3001/restaurant`, Restaurant)
+      .then(res => dispatch({
+        type: 'ADD_RESTAURANT',
+        payload: res.data
+      })
+      ).catch(err => {
+        console.error(err)
+      });
+  }
+}
+
+export function deleteRestaurant(id) {
+  return (dispatch) => {
+    axios.delete(`http://localhost:3001/restaurant/${id}`)
+    .then(response => {
+       dispatch({
+        type: DELETE_RESTAURANT,
+        payload: id
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+}
+
+UPDATE_RESTAURANT
+export async function editRestaurant(id, payload) {
+  // console.log('entra aca')
+  await axios.put("http://localhost:3001/admin/productos/" + id, payload)
+    .then((response) => {
+      if (response) alert('El producto se modificó correctamente');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+export function searchRecipeDetail(recipeId) {
+  return function (dispatch) {
+    axios.get(`http://localhost:3001/recipes/${recipeId}`)
+     .then(res => dispatch({
+        type: 'SEARCH_RECIPE_DETAIL',
+        payload: res.data
+      })
+      ).catch(err => {
+        console.error(err)
+      });
+  };
+};
+
+
+export function getDiets() {
+  return function (dispatch) {
+    axios.get(`http://localhost:3001/types`)
+     .then(res => dispatch({
+        type: 'ALL_DIETS',
+        payload: res.data
+      })
+      ).catch(err => {
+        console.error(err)
+      });
+  };
+};
