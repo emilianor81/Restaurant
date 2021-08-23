269c733bd7b3df3519ca125b1b38c1dbe92625f8
Santiago Soares Gache <santisoaresgache@gmail.com>
2021-08-23 14:28:26 -0300

    index force esta en false

--- api/src/routes/restaurants.js
+++ api/src/routes/restaurants.js
@@ -1,9 +1,21 @@
 const { Router } = require('express');
-const router = Router();
+const {Restaurant} = require('../db');
 
-const addRestaurant = router.post('/', (req, res)=>{
+const router = Router();
+let id =1
+const addRestaurant = router.post('/', async (req, res)=>{
 //    const {title, summary, spoonacularScore, healthScore, instructions, diets}=req.body
     console.log(req.body)
+    const {title, city, description, adress, picture} = req.body
+    const newRestaurant = { nombre: title, descripcion: description, ciudad: city, direccion: adress, foto: picture, id: id++ }
+    try{
+        const createRestaurant= await Restaurant.create(newRestaurant)
+        res.send(newRestaurant)
+    }
+    catch(err){
+        res.send(err)
+    }
+
 })
 
 
