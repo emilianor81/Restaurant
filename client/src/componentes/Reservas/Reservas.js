1a061497cb3d8aae6d134e2a6a0a55dc94241277
Santiago Soares Gache <santisoaresgache@gmail.com>
2021-08-25 14:30:20 -0300

    .

--- client/src/componentes/Reservas/Reservas.js
+++ client/src/componentes/Reservas/Reservas.js
@@ -15,11 +15,9 @@ export default function Reservas({ match }) {
   const submit = () => {
     // console.log('hola no disp', JSON.stringify(startDate).split('T')[0], noDisponible[0])
     let a = noDisponible.filter(e => e.fecha.includes(JSON.stringify(startDate).split('T')[0].slice(1)));
-    console.log(a)
-    if (a) {
+    if (a.length) {
       return alert('fecha no disponible '+a[0].fecha)
     } else {
-      console.log(1, a);
       dispatch(addReservation([startDate, match.params.id]));
       alert('guarde su fecha de reserva ' + JSON.stringify(startDate).split('T')[0].concat('"'));
       history.push('/')
